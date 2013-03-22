b361be9e05ee1733da05e43afe3744a9dac78c4f
Tim Lupfer <tim@sundaysenergy.com>
2013-03-22 11:43:58 -0500

    Facebook

--- themes/watchmn/javascripts/custom.js
+++ themes/watchmn/javascripts/custom.js
@@ -36,7 +36,7 @@ $.getJSON('http://twitter.supersimple.co/facebook?user=watchmn', function(data) 
         $("#da-slide" + counter + " p").html(post.message);
         $("#da-slide" + counter + " .da-img").html("<img src=\"" + post.picture + "\" alt=\"" + post.name + "\" />");
         if (post.name.length > 30) {
-          var new_title = post.name.substring(30);
+          var new_title = post.name.substring(0,30);
           post.name = new_title + "...";
         }
         $("#da-slide" + counter + " h2").html(post.name);
